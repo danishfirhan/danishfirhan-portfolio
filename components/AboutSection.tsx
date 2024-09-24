1ffe36ec418959b8360710a8242b2051ddf958e9
Danish Firhan <danishfirhan@graduate.utm.my>
2024-09-24 16:52:17 +0800

    Edited Hero and About Section

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -80,8 +80,8 @@ const AboutSection = () => {
               alt=""
               width={325}
               height={325}
-              className="hidden md:block md:relative md:bottom-4 md:left-32 md:z-0"
-            />
+              className="hidden md:block md:relative md:bottom-4 md:left-20 md:z-0 mt-8"
+              />
           </div>
         </div>
       </div>
